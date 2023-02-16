652d335d8c6de45bf84d7a773d87b43e54a755f5
timiajeojo <timiajeojo@gmail.com>
2023-02-16 05:58:52 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program 2
+ * Return 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)s
+sizeof(a));
+print("Size of an int: %lu byte(s)\n", (unsigned long)s
+sizeof(b));("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
+}

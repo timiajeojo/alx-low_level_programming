9d456cf3759465365a151dc322649a10ccd50fde
timiajeojo <timiajeojo@gmail.com>
2023-02-17 05:13:51 -0800

    tas

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
     int i;
-    
+
     for (i = 0; i < 10; i++)
     {
         putchar(i + '0');

3acca41a24ec54e60b0ade08f52e2f7c4b339956
timiajeojo <timiajeojo@gmail.com>
2023-02-17 03:43:03 -0800

    task2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,25 +9,54 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %d is ", n);
+    printf("Last digit of %d is ", n);
 
-	if (n < 0)
-		n *= -1;
+    if (n < 0)
+        n *= -1;
 
-	n = n % 10;
+    n = n % 10;
 
-	if (n == 0)
-		printf("0");
-	else if (n > 5)
-		printf("%d and is greater than 5\n", n);
-	else
-		printf("%d and is less than 6 and not 0\n", n);
+    if (n == 0)
+        printf("0");
+    else if (n > 5)
+        printf("%d and is greater than 5\n", n);
+    else
+        printf("%d and is less than 6 and not 0\n", n);
 
-	return (0);
+    /* Example for n = 980 */
+    n = 980;
+    printf("Last digit of %d is ", n);
+
+    n = n % 10;
+
+    if (n == 0)
+        printf("0");
+    else if (n > 5)
+        printf("%d and is greater than 5\n", n);
+    else
+        printf("%d and is less than 6 and not 0\n", n);
+
+    /* Example for n = -98 */
+    n = -98;
+    printf("Last digit of %d is ", n);
+
+    if (n < 0)
+        n *= -1;
+
+    n = n % 10;
+
+    if (n == 0)
+        printf("0");
+    else if (n > 5)
+        printf("%d and is greater than 5\n", n);
+    else
+        printf("%d and is less than 6 and not 0\n", n);
+
+    return (0);
 }
 

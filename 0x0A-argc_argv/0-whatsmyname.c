5e7b335bd581d8d023d868a1b3761454cf421383
timiajeojo <timiajeojo@gmail.com>
2023-03-10 13:03:03 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
 * Return: Always 0 (Success)
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), 
+char *argv[])
 {
 printf("%s\n", *argv);
 return (0);

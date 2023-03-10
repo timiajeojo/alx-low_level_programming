70b38668693f64fc33155eb5b2e0dccd4c0a7685
timiajeojo <timiajeojo@gmail.com>
2023-03-10 12:41:30 -0800

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,10 +8,8 @@
 *
 * Return: Always 0 (Success)
 */
-				
-int main(int argc, char *argv[])
-{
-(void) argv; /*Ignore argv*/
-printf("%d\n", argc - 1);
-return (0);
+
+int main(int argc, char *argv[]) {
+    printf("%d\n", argc-1);
+    return 0;
 }

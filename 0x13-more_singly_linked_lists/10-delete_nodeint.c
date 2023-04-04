9303f815ced53139fff3ba0b1c91e7effea6d436
timiajeojo <timiajeojo@gmail.com>
2023-04-03 23:48:17 -0700

    done task

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,5 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ 
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of a linnked list
+ * add_nodeint_end - adds a node at the end of a linked list
  * @head: pointer to the first element in the list
- * @n: data to instert in the new element
+ * @n: data to insert in the new element
  *
- * Return: pointer to th new node, or NULL if it fails
+ * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new;
 listint_t *temp = *head;
+
 new = malloc(sizeof(listint_t));
 if (!new)
 return (NULL);
